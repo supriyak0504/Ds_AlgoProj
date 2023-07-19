95bea35377c72b1582d89c9aa552337a59826268
unknown <k.supriya0504@gmail.com>
2023-07-19 17:22:07 -0500

    The ds algo working project

---
+++ src/test/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+import org.testng.annotations.*;
+
+public class Main {
+
+     @BeforeTest
+     public void base() {
+
+     }
+
+     @Test
+     public void test() {
+     }
+
+     @AfterTest
+     public void teardown() {
+
+     }
+
+     @AfterTest
+     public void teardown1() {
+
+     }
+}
